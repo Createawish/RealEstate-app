dc49e175636bf632c447cdc26381a3530043e55c
Createawish <bezlyudovalera1996@gmail.com>
2022-09-07 08:59:42 +0300

    Feature: add main img

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import NavBar from "./components/navBar/NavBar";
+import Hero from "./components/hero/Hero";
 
 function App() {
   return (
    <>
        <NavBar/>
+       <Hero/>
      </>
   );
 }
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import style from './Hero.module.css'
+import {AiOutlineSearch} from "react-icons/ai";
+
+const Hero = () => {
+    return (
+        <div className={style.hero}>
+            <div className={style.content}>
+                <h1>Find the perfect place</h1>
+                <p className={style.search}>Search  the lagest selection of luxury high-rise apartments, multi-family homes, and luxury homes.</p>
+            <form className={style.form}>
+                <div>
+                    <input type='text' placeholder='Enter Keyword...'/>
+                </div>
+                <div className={style.radio}>
+                    <input type='radio' checked/>
+                    <label>Buy</label>
+                    <input type='radio' />
+                    <label>Rent</label>
+                    <button type="submit">
+                        <AiOutlineSearch className={style.icon}/>
+                    </button>
+                </div>
+            </form>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
